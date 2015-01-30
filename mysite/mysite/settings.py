00ffa5511d234980b1ba76ba8f4c1569f410b0c0
Alberto Morales <amd77@galotecnia.com>
2015-01-30 13:00:18 +0000

    Changed timezone and language

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -66,9 +66,9 @@ DATABASES = {
 # Internationalization
 # https://docs.djangoproject.com/en/1.7/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es-es'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Atlantic/Canary'
 
 USE_I18N = True
 
