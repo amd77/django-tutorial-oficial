bd1096f4be770ab59ce07072b96fb7fc767d25b6
Alberto Morales <amd77@galotecnia.com>
2015-01-30 13:15:03 +0000

    Customizing admin form: adding related-simple way

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -1,12 +1,6 @@
 from django.contrib import admin
-from polls.models import Question
+from polls.models import Choice, Question
 
 
-class QuestionAdmin(admin.ModelAdmin):
-    fieldsets = [
-        (None,               {'fields': ['question_text']}),
-        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
-    ]
-
-
-admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice)
+admin.site.register(Question)
