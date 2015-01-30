19a65dc5b032a1b4d670f8282435ef01cd788b48
Alberto Morales <amd77@galotecnia.com>
2015-01-30 13:35:35 +0000

    Second template, using get_object_or_404

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,5 +1,4 @@
-from django.http import Http404
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse
 
 from polls.models import Question
@@ -12,10 +11,7 @@ def index(request):
 
 
 def detail(request, question_id):
-    try:
-        question = Question.objects.get(pk=question_id)
-    except Question.DoesNotExist:
-        raise Http404("Question does not exist")
+    question = get_object_or_404(Question, pk=question_id)
     return render(request, 'polls/detail.html', {'question': question})
 
 
