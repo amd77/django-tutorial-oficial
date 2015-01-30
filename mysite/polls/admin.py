58e9c40c79312fa024ecea6117f17d034114e2dd
Alberto Morales <amd77@galotecnia.com>
2015-01-30 13:15:42 +0000

    Customizing admin form: adding related-with inlines

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -2,5 +2,16 @@ from django.contrib import admin
 from polls.models import Choice, Question
 
 
-admin.site.register(Choice)
-admin.site.register(Question)
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 3
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+    ]
+    inlines = [ChoiceInline]
+
+admin.site.register(Question, QuestionAdmin)
