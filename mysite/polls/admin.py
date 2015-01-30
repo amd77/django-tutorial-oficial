9c34dae663a959575b51c916bdced5f0dbfdfc46
Alberto Morales <amd77@galotecnia.com>
2015-01-30 13:11:11 +0000

    Customizing admin form: reordering fields

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
 from polls.models import Question
 
-admin.site.register(Question)
+
+class QuestionAdmin(admin.ModelAdmin):
+    fields = ['pub_date', 'question_text']
+
+
+admin.site.register(Question, QuestionAdmin)
